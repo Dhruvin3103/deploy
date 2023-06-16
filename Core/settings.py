db88385139fc664ba59717cf65e8c1b9a43a3a72
Hemant <dhruvinhemant5@gmail.com>
2023-06-16 04:41:53 -0700

    mailSystem-added

--- Core/settings.py
+++ Core/settings.py
@@ -138,6 +138,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -254,3 +255,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
--- prodhouse/views.py
+++ prodhouse/views.py
@@ -3,6 +3,9 @@ from .models import Portfolio,Contactus
 from .serializer import PortfolioSerializer,ContactusSerializer
 from  rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin,CreateModelMixin
+from django.core.mail import send_mail
+from django.conf import settings
+from rest_framework.response import Response
 # Create your views here.
 
 class PortfolioAPI(GenericAPIView, ListModelMixin, CreateModelMixin):
@@ -17,5 +20,13 @@ class ContactusAPI(GenericAPIView, CreateModelMixin):
     serializer_class = ContactusSerializer
     queryset = Contactus.objects.all()
     def post(self, request):
-        #email
+        try:
+            subject = 'Some one contacted you!!'
+            print(request.data)
+            message = f"NAME:{request.data['name']}\nEMAIL:{request.data['email']}\nPHONE:{request.data['phone']}\nMESSAGE:{request.data['message']}"
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = ['gajjoshi2003@gmail.com']
+            send_mail( subject, message, email_from, recipient_list )
+        except Exception as e:
+            return Response({'status': 403, 'message': 'Sorry Some error has occured', 'error':str(e)})
         return self.create(request)
